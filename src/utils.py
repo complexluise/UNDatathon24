532f71261966021b9c188695395a4237f340439c
complexluise <lhiguerac@unal.edu.co>
2024-11-11 09:50:26 -0500

    CIIU for analysis

---
+++ src/utils.py
@@ -0,0 +1,38 @@
+
+
+ciiu_interes = {
+    "alojamientos": [
+        "5511",  # Alojamiento en hoteles.
+        "5512",  # Alojamiento en apartahoteles.
+        "5513",  # Alojamiento en centros vacacionales.
+        "5514",  # Alojamiento rural.
+        "5519",  # Otros tipos de alojamiento para visitantes.
+        "5520",  # Actividades de zonas de camping y parques para vehículos recreacionales.
+        "5530",  # Servicio de estancia por horas.
+        "5590"  # Otros tipos de alojamiento n.c.p.
+    ],
+    "servicios_turisticos": [
+        "7911",  # Actividades de las agencias de viaje.
+        "7912",  # Actividades de operadores turísticos.
+        "7990"  # Otros servicios de reserva y actividades relacionadas.
+        ],
+    "entretenimiento": [
+        "3012",  # Construcción de embarcaciones de recreo y deporte
+        "8230",  # Organización de convenciones y eventos comerciales
+        "9103",  # Actividades de jardines botánicos, zoológicos y reservas naturales
+        "9200",  # Actividades de juegos de azar y apuestas
+        "9321",  # Actividades de parques de atracciones y parques temáticos
+        "9329"  # Otras actividades recreativas y de esparcimiento n.c.p.
+    ],
+    "transporte": [
+        "4911",  # Transporte férreo de pasajeros
+        "4921",  # Transporte de pasajeros
+        "5011",  # Transporte de pasajeros marítimo y de cabotaje
+        "5021",  # Transporte fluvial de pasajeros
+        "5111",  # Transporte aéreo nacional de pasajeros
+        "5112",  # Transporte aéreo internacional de pasajeros
+    ],
+    "alimentacion": [
+        "5619",  # Otros tipos de expendio de comidas preparadas n.c.p.
+    ]
+}
